c092b03aeba93b0b11edb3300500fdbd8cdaffa1
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-20 10:40:02 -0700

    Code cleanup; stubbed out CRUD routes.

--- server/app.js
+++ server/app.js
@@ -2,18 +2,29 @@ const path = require('path');
 const express = require('express');
 const db = require('../database/db.js');
 
-let app = express();
+const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.get('/restaurant/:restaurant_id/:date', function(req, res) {
-  db.grabTimeSlots(req.params.restaurant_id, req.params.date, function(error, data) {
+app.get('/restaurant/:restaurant_id/:date', (req, res) => {
+  db.grabTimeSlots(req.params.restaurant_id, req.params.date, (error, data) => {
     if (error) {
       res.sendStatus(500);
     }
-    res.send(data); 
-  }); 
-});  
-   
+    res.send(data);
+  });
+});
+
+app.post('/restaurant/:restaurant_id/:date', (req, res) => {
+  res.end();
+});
+
+app.put('/restaurant/:restaurant_id/:date', (req, res) => {
+  res.end();
+});
+
+app.delete('/restaurant/:restaurant_id/:date', (req, res) => {
+  res.end();
+});
+
 module.exports = app;
- 
